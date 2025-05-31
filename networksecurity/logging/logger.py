9a7b6819ea190811a6459d8b53c89ad834c67d27
Shahinur <shahinhasanronys@gmail.com>
2025-05-31 08:44:57 +0600

    mongodb is connected

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -3,7 +3,12 @@ import os
 from datetime import datetime
 LOG_FILE=f"{datetime.now().strftime('%d-%m-%Y-%H-%M-%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs")
-os.mkdir(logs_path,exist_ok=True)
+
+
+
+os.makedirs(logs_path, exist_ok=True)
+
+
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
