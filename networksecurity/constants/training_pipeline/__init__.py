22b11be7527f07c2525573ea8a40b403ac7a58e2
Shahinur <shahinhasanronys@gmail.com>
2025-06-02 11:04:45 +0600

    data validation added

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -7,10 +7,10 @@ TARGET_COLUMN="Result"
 PIPELINE_NAME: str="NetworkSecurity"
 ARTIFACT_DIR:str="artifact"
 FILE_NAME:str="phisingData.csv"
-TRAIN_PATH_NAME: str="train.csv"
+TRAIN_FILE_NAME: str="train.csv"
 TEST_FILE_NAME: str="test.csv"
 
-
+SCHEMA_FILE_PATH: os.path.join("data_schema","schema.yml")
 
 DATA_INGESTION_COLLECTION_NAME: str="NetworkData"
 DATA_INGESTION_DATABASE_NAME: str="network_security"
@@ -18,5 +18,13 @@ DATA_INGESTION_DIR_NAME: str="data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str="feature_store"
 DATA_INGESTION_INGESTION_DIR: str="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=.2
+
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 
