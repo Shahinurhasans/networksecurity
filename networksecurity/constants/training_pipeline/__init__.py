ef8eb51d75c058b84bfe2c9fab824fb4d789a19c
Shahinur <shahinhasanronys@gmail.com>
2025-06-05 08:51:47 +0600

    evaluation added

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -20,6 +20,9 @@ DATA_INGESTION_INGESTION_DIR: str="ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float=.2
 
 
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
 
 """
 Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
@@ -53,4 +56,21 @@ DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
 PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
 
+
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
+
+"""
+Model Trainer ralated constant start with MODE TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD: float = 0.0
+
+TRAINING_BUCKET_NAME = "netwworksecurity"
 
