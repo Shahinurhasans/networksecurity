4067df2866d6fae799f6cb399d259c578a6005c6
Shahinurhasans <95462455+Shahinurhasans@users.noreply.github.com>
2026-05-02 00:15:26 +0600

    data ingestion arficat done

---
+++ main.py
@@ -0,0 +1,22 @@
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+
+import sys
+
+if __name__ == '__main__':
+    try:
+        trainingpipelineconfig = TrainingPipelineConfig()
+
+        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
+        logging.info("initiated the data ingestion config")
+
+        data_ingestion_obj = DataIngestion(dataingestionconfig)
+
+        dataingestionartifact = data_ingestion_obj.initiate_data_ingestion()
+
+        print(dataingestionartifact)
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
